f5a157f083ab0176875639ec2e7feef9ef25070f
John Peterson <vengefulvidarr@gmail.com>
2017-10-19 12:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var name = "Susan"
 var height = "74"
-
-
-// Don't worry about this
-module.exports = { name, height
-}
-
-// Test variables
 if (name === "Susan") {
   console.log("The name is correct")
 } else {
   console.log("Expected name is Susan")
+}
+
+// Don't worry about this
+module.exports = { name, height
 }
